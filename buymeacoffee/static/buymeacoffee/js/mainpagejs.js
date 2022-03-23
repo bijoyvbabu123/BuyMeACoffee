a045d7707bf0285ffd71b8ee184f972934330e03
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2022-03-23 15:33:53 +0530

    setup basic project structure and most basic single button web page.

---
+++ buymeacoffee/static/buymeacoffee/js/mainpagejs.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function () {
+    alert("It is workingggg");
+    $("#buybutton").fadeOut("slow");
+    $("#buybutton").fadeIn("slow");
+})
--- buymeacoffee/views.py
+++ buymeacoffee/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    context = {}
+    return render(request, 'buymeacoffee/mainpage.html', context)
